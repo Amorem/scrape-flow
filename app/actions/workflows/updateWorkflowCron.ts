cde5bc1ee62b1d5eb297bfc3f4f07341fa56a6f2
AMOREM <dev@amorem.com>
2024-11-14 16:28:33 +0100

    cron UI

---
+++ app/actions/workflows/updateWorkflowCron.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import parser from "cron-parser";
+import { revalidatePath } from "next/cache";
+
+export async function UpdateWorkflowCron({
+  id,
+  cron,
+}: {
+  id: string;
+  cron: string;
+}) {
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Unauthenticated");
+  }
+
+  try {
+    const interval = parser.parseExpression(cron, { utc: true });
+    await prisma.workflow.update({
+      where: {
+        userId,
+        id,
+      },
+      data: {
+        cron,
+        nextRunAt: interval.next().toDate(),
+      },
+    });
+  } catch (error: any) {
+    console.error("invalid cron:", error);
+    throw new Error("Invalid cron expression");
+  }
+
+  revalidatePath(`/workflows`);
+}
