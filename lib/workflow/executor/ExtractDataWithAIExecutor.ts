113b11c6cd841a417e6e9a56772dd57e45747c16
AMOREM <dev@amorem.com>
2024-11-20 13:35:04 +0100

    OpenAI API integration

--- lib/workflow/executor/ExtractDataWithAIExecutor.ts
+++ lib/workflow/executor/ExtractDataWithAIExecutor.ts
@@ -1,8 +1,8 @@
 import { ExecutionEnvironment } from "@/types/executor";
-import { waitFor } from "@/lib/helper/waitFor";
 import { ExtractDataWithAITask } from "../task/ExtractDataWithAI";
 import prisma from "@/lib/prisma";
 import { symmetricDecrypt } from "@/lib/encryption";
+import OpenAI from "openai";
 
 export async function ExtractDataWithAIExecutor(
   environment: ExecutionEnvironment<typeof ExtractDataWithAITask>
@@ -45,12 +45,48 @@ export async function ExtractDataWithAIExecutor(
       return false;
     }
 
-    const mockExtractedData = {
-      usernameSelector: "#username",
-      passwordSelector: "#password",
-      loginSelector: "body > div.container > form > input.btn.btn-primary",
-    };
-    environment.setOutput("Extracted data", JSON.stringify(mockExtractedData));
+    // const mockExtractedData = {
+    //   usernameSelector: "#username",
+    //   passwordSelector: "#password",
+    //   loginSelector: "body > div.container > form > input.btn.btn-primary",
+    // };
+
+    const openAI = new OpenAI({
+      apiKey: plainCredentialValue,
+    });
+
+    const response = await openAI.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a webscraper helper that extracts data from HTML or text. You will be given a piece of text or HTLM content as input and also the prompt with the data you want to extract. The response should always be only the extracted data as a JSON array or object, without any additional words or explanations. Analyse the input carefully and extract data precisely based on the prompt. If no data is found, return an empty JSON array. Work only with the provided content and ensure the output is always a valid JSON array without any surrounding text",
+        },
+        {
+          role: "user",
+          content: content,
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 1,
+    });
+
+    environment.log.INFO(`Prompt tokens: ${response.usage?.prompt_tokens}`);
+    environment.log.INFO(
+      `Completition tokens: ${response.usage?.completion_tokens}`
+    );
+
+    const result = response.choices[0].message?.content;
+    if (!result) {
+      environment.log.ERROR("empty return from AI");
+      return false;
+    }
+
+    environment.setOutput("Extracted data", result);
 
     return true;
   } catch (error: any) {
